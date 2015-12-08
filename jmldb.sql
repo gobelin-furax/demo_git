d278c93770f235500d3ef805507b9ff56ed476a2
gobelin-furax <julienmarcelino@hotmail.fr>
2015-12-08 15:21:25 +0100

    ajout de la table de la base de données

---
+++ jmldb.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Sam 31 Octobre 2015 à 14:47
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `jmldb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tabletchat`
+--
+
+CREATE TABLE IF NOT EXISTS `tabletchat` (
+  `idmessage` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(30) NOT NULL,
+  `message` text NOT NULL,
+  `datemessage` datetime NOT NULL,
+  PRIMARY KEY (`idmessage`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
